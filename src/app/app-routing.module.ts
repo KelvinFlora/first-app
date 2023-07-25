60fc41aa4fc61ad4d8c1f02f0cd4550206adeda9
Kelvin Flora <kelvin.flora@gmail.com>
2023-07-25 17:36:39 -0300

    order and cart route added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,27 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { OrderPage } from './order/order.page';
 
 const routes: Routes = [
   {
     path: '',
     loadChildren: () =>
       import('./tabs/tabs.module').then((m) => m.TabsPageModule),
-  },  {
-    path: 'order',
-    loadChildren: () => import('./order/order.module').then( m => m.OrderPageModule)
   },
-
+  {
+    path: 'order',
+    loadChildren: () =>
+      import('./order/order.module').then((m) => m.OrderPageModule),
+  },
+  {
+    path: 'order/:id',
+    component: OrderPage,
+  },
+  {
+    path: 'cart',
+    loadChildren: () =>
+      import('./cart/cart.module').then((m) => m.CartPageModule),
+  },
 ];
 @NgModule({
   imports: [
