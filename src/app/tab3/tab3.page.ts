6b4de45280d8266e51a33ab1db47f0f535b81b53
Kelvin Flora <kelvin.flora@gmail.com>
2023-07-25 17:37:28 -0300

    emoji function implemented

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab3',
@@ -6,7 +7,7 @@ import { Component } from '@angular/core';
   styleUrls: ['tab3.page.scss'],
 })
 export class Tab3Page {
-  constructor() {}
+  constructor(private toastController: ToastController) {}
 
   message: string = '';
   messages: string[] = [];
@@ -16,5 +17,29 @@ export class Tab3Page {
       this.messages.push(this.message);
       this.message = '';
     }
+  }
+
+  async showToast(message: string) {
+    const toast = await this.toastController.create({
+      message: message,
+      duration: 2000,
+      position: 'bottom',
+    });
+    toast.present();
+  }
+
+  async abrirTecladoEmoji() {
+    const input = document.createElement('input');
+    input.setAttribute('type', 'text');
+    input.setAttribute('style', 'position: absolute; top: -100px;');
+    document.body.appendChild(input);
+
+    input.focus();
+    input.setAttribute('type', 'emoji');
+
+    input.addEventListener('blur', () => {
+      document.body.removeChild(input);
+      this.showToast('Teclado de emoji fechado!');
+    });
   }
 }
